b7f74293c1d762643b0eef21b3be82c422299a6d
Subeen Regmi <subeenregmi123@gmail.com>
2023-11-07 19:46:37 +0000

    finished solver added some puzzles to test it on

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import copy
-
 def createSudokuBoard(filename):
 	board = []
 	file = None
@@ -20,21 +18,22 @@ def createSudokuBoard(filename):
 	file.close()
 	return board
 
-def checkIfInvalidBoard(board):
+def checkIfValid(board):
 	for i in range(9):
 		current_row = set({})
 		current_col = set({})
 		for j in range(9):
-			if (board[i][j] == 0) or (board[j][i] == 0):
-				continue
-
 			if board[i][j] in current_row:
 				return False
+			else:
+				if board[i][j] != 0:
+					current_row.add(board[i][j])
+
 			if board[j][i] in current_col:
 				return False
-
-			current_row.add(board[i][j])
-			current_col.add(board[j][i])
+			else:
+				if board[j][i] != 0:
+					current_col.add(board[j][i])
 	
 	for i in range(3):
 		for j in range(3):
@@ -50,13 +49,37 @@ def checkIfInvalidBoard(board):
 	return True 
 
 def solveSudoku(board):
+	if not checkIfValid(board):
+		print("Invalid starting board")
+		return
+
 	solved = False
-	solvingb = copy.deepcopy(board)
-	i = 0 
-	j = 0 
-	while not solved:
-		solvingb[i][j] += 1
-		if solvingb[i][j] >= 10:
-			solvingb[i][j] = 0
-			if j-1 < 0:
-				
+	index = 0
+	zeros = []
+	for i in range(9):
+		for j in range(9):
+			if board[i][j] == 0:
+				zeros.append((i, j))
+
+	while True:
+		board[zeros[index][0]][zeros[index][1]]	+= 1
+		
+		if board[zeros[index][0]][zeros[index][1]] > 9:
+			board[zeros[index][0]][zeros[index][1]] = 0
+			index = index - 1
+			continue
+
+		if not checkIfValid(board):
+			continue	
+
+		else:
+			index += 1
+			if index == len(zeros):
+				break
+			
+	return board
+
+b = createSudokuBoard("hardest.txt")
+x = solveSudoku(b)
+print(x)
+print(checkIfValid(x))
